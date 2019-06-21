633c8f89acab6d1445c2cc46a09e5a0d8ab04304
Dustin Anderson <dkanderson@gmail.com>
2019-06-21 14:46:11 -0500

    wrapping up

--- src/App.js
+++ src/App.js
@@ -20,63 +20,7 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 // this uses the themeing from material ui and creates what 
 // is needed for the `classes` object used to style components
 // you can add a class here and use it in the JSX below
-const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    title: {
-      flexGrow: 1,
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      width: theme.spacing(7),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      color: 'inherit',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 7),
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: 120,
-        '&:focus': {
-          width: 200,
-        },
-      },
-    },
-    select: {
-      backgroundColor: 'white',
-      width: '200px',
-      height: '63px',
-      padding: '20px',
-      marginBottom: '15px',
-      marginLeft: '20px'
-    }
-  }));
+
 
  function App() {
     const [searchString, setSearchString] = useState("React");
@@ -94,25 +38,80 @@ const useStyles = makeStyles(theme => ({
     async function fetchData() {
         const unfilteredResult = await axios('https://hn.algolia.com/api/v1/search?query=' + searchString);
         // only show items that are recent to a given year
-        console.log("HERE",unfilteredResult.data);
-        unfilteredResult.data.hits = unfilteredResult.data.hits.filter(item => new Date(item.created_at).getFullYear() > new Date(recency) )
+        unfilteredResult.data.hits = unfilteredResult.data.hits.filter(item => new Date(item.created_at).getFullYear() >= new Date(recency) )
         setData(unfilteredResult.data);
     }
 
+    //refetch data if the searchString changes or the recency changes
     useEffect(() => {
- 
         fetchData();
         return() => {};
     }, [searchString, recency]);
 
+    //refetch data every minute
     useEffect(() => {
-        const timerId = setInterval( () => fetchData(), 6000);
-        
+        const timerId = setInterval( () => fetchData(), 60000);  
         return function cleanup(){
             clearInterval(timerId);
         }
     }, );
-
+    const useStyles = makeStyles(theme => ({
+      root: {
+        flexGrow: 1,
+      },
+      title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+          display: 'block',
+        },
+      },
+      search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      },
+      searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      inputRoot: {
+        color: 'inherit',
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: 120,
+          '&:focus': {
+            width: 200,
+          },
+        },
+      },
+      select: {
+        backgroundColor: 'white',
+        width: '200px',
+        height: '63px',
+        padding: '20px',
+        marginBottom: '15px',
+        marginLeft: '20px'
+      }
+    }));
     const classes = useStyles();
 
     return(
@@ -163,7 +162,6 @@ const useStyles = makeStyles(theme => ({
                   </FormControl>
           </Toolbar>
         </AppBar>
-        {JSON.stringify(data)}
         <DataCards data={data}></DataCards>
     </div>  
     )
